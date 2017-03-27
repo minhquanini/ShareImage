1a3d6a492687dd6e1479b09ef8e0c42236b961e6
minhquanini <minhquanmessi@gmail.com>
2017-03-28 00:20:21 +0700

    đăng ảnh và hiển thị thành công

--- ShareImage/Controllers/HomeuserController.cs
+++ ShareImage/Controllers/HomeuserController.cs
@@ -1,5 +1,6 @@
 ﻿using Model.Dao;
 using Model.EF;
+using ShareImage.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,7 +17,33 @@ namespace ShareImage.Controllers
         {
             var db = new ShareImageDbContext();
 
-            var model = db.Posts.ToList();
+            //var model = db.Posts.ToList();
+            List<PostViewModel> model =new List<PostViewModel>();
+
+            var q = (from a in db.Posts
+                     join b in db.Users on a.UserID equals b.UserID 
+                     //from rt in t.DefaultIfEmpty()
+                     select new
+                     {
+                         postID=a.PostID,
+                         title=a.Title,
+                         description=a.Description,
+                         createDate=a.CreateDate,
+                         picture=a.Picture,
+                         username=b.Username,
+                     }).ToList();
+            foreach (var item in q)
+            {
+                model.Add(new PostViewModel()
+                {
+                    PostID = item.postID,
+                    Title=item.title,
+                    Description=item.description,
+                    Picture=item.picture,
+                    CreateDate=item.createDate,
+                    Username=item.username,
+                });
+            }
 
             return View(model);
             //return View(q.OrderBy(x => x.CreateDate).ToList());
